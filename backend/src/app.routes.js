e2029040b35bc7eb28c22c92145f3ada1bdd7ffd
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-16 16:30:30 +0700

    now

--- backend/src/app.routes.js
+++ backend/src/app.routes.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const clc = require("cli-color");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const UserModel = require("./models/User");
 const ScheduleModel = require("./models/Schedule");
@@ -38,15 +39,100 @@ router.get("/", (req, res, next) => {
   });
 });
 
-router.post("/auth/register", (req, res, next) => {
+
+
+//TODO: AUTHENTICATIONS
+router.post("/auth/register", async (req, res, next) => {
   try {
+    const { name, email, password } = req.body;
+
+    // Validate input
+    if (!name || !email || !password) {
+      return res.status(400).json({ error: "All fields are required." });
+    }
+
+    const existUser = await UserModel.findOne({ email });
+
+    if (existUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const newUser = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "15d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 15 * 24 * 3600000,
+      sameSite: "strict",
+    });
+
+    const { password: _, ...userWithoutPassword } = savedUser.toObject();
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      user: userWithoutPassword,
+    });
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/auth/login", (req, res, next) => {
+router.post("/auth/login", async (req, res, next) => {
   try {
+    const { email, password } = req.body;
+
+    // Validate input
+    if (!email || !password) {
+      return res.status(400).json({ error: "All fields are required." });
+    }
+
+    const existUser = await UserModel.findOne({ email });
+
+    if (!existUser) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+    const isMatchPassword = await bcrypt.compare(password, existUser.password);
+    if (isMatchPassword) {
+      return res.status(400).json({
+        message: "Wrong password",
+      });
+    }
+
+    const token = jwt.sign({ id: existUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "15d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 15 * 24 * 3600000,
+      sameSite: "strict",
+    });
+
+    const { password: _, ...userWithoutPassword } = existUser.toObject();
+
+    return res.status(201).json({
+      message: "User login successfully",
+      user: userWithoutPassword,
+    });
   } catch (error) {
     next(error);
   }
@@ -62,7 +148,28 @@ router.get("/auth/logout", (req, res) => {
   res.status(200).json({ message: "Logged out successfully" });
 });
 
+//TODO: PROFILE
 router.get("/get-profile/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = UserModel.findById(id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.json({
+      message: "Successful",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/change-profile-image/:id", (req, res, next) => {
   try {
   } catch (error) {
     next(error);
@@ -76,6 +183,7 @@ router.put("/change-profile/:id", (req, res, next) => {
   }
 });
 
+//TODO: TARGET
 router.put("/update_health/:id", (req, res, next) => {
   try {
   } catch (error) {
@@ -97,6 +205,7 @@ router.put("/update_gain_weigh/:id", (req, res, next) => {
   }
 });
 
+//TODO: NUTRITION
 router.get("/menu-day", (req, res, next) => {
   try {
   } catch (error) {
@@ -111,6 +220,7 @@ router.get("/menu-week", (req, res, next) => {
   }
 });
 
+//TODO: SCHEDULE
 router.post("/create-exercise", (req, res, next) => {
   try {
   } catch (error) {
