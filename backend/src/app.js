eade26a9b3f43a9f771552511f5d5cc505dd1e68
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-20 21:08:51 +0700

    now

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,4 +29,9 @@ app.use("/", express.static(path.join(__dirname, "dist")));
 
 app.use("/api", router);
 
+// Rewrite and redirect all other requests to React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 module.exports = app;
